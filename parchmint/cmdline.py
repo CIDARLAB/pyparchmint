a9bb59b555ffe10b00aed7907289e508d7ef1516
Radhakrishna Sanka <rkrishnasanka@gmail.com>
2020-11-17 13:16:39 -0500

    Updated the command line interface for parchmint validation

--- parchmint/cmdline.py
+++ parchmint/cmdline.py
@@ -1,6 +1,14 @@
+from pathlib import Path
 from parchmint.device import Device
+import argparse
 
+def validate_V1():
+    parser = argparse.ArgumentParser()
 
-def validate_V1(file_path):
+    parser.add_argument(
+        'input', help="This is the file thats used as the input ")
+
+    args = parser.parse_args()
+    file_path = Path(args.input).resolve()
     with open(file_path) as data_file:
         Device.validate_V1(data_file.read())
